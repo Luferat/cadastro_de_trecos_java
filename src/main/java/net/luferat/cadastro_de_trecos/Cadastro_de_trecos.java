1b8cdc118136546244ec4a18cb1b125e59a387f7
André Luferat <contato@luferat.net>
2023-05-23 20:39:26 -0300

    fghjkl

--- src/main/java/net/luferat/cadastro_de_trecos/Cadastro_de_trecos.java
+++ src/main/java/net/luferat/cadastro_de_trecos/Cadastro_de_trecos.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class Cadastro_de_trecos {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        mainMenu();
+    }
+
+    // Método que exibe o menu principal.
+    public static void mainMenu() {
+        clearScreen();
         System.out.println("Cadastro de Trecos\n");
         System.out.println("Menu:");
         System.out.println("\t[1] Listar todos");
@@ -15,12 +23,74 @@ public class Cadastro_de_trecos {
         System.out.println("\t[0] Sair");
         System.out.print("\nOpção: ");
 
-        Scanner scanner = new Scanner(System.in);
+        // Recebe a opção do teclado.
         String option = scanner.next();
-        
-        if(option.length() != 1) {
-            System.out.println("Bizonhou!!!");
-        }
 
+        // Executa um método conforme a opção escolhida.
+        switch (option) {
+            case "0":
+                exitProgram();
+                break;
+            case "1":
+                listAll();
+                break;
+            case "2":
+                listOne();
+                break;
+            case "3":
+                newThing();
+                break;
+            case "4":
+                editThing();
+                break;
+            case "5":
+                deleteThing();
+                break;
+            default:
+                reloadMenu();
+        }
     }
+
+    // Encerra o programa.
+    public static void exitProgram() {
+        scanner.close();
+        clearScreen();
+        System.out.println("\n\nFui!\n\n");
+        System.exit(0);
+    }
+
+    // Lista todos os trecos cadastrados.
+    public static void listAll() {
+    }
+
+    // Lista um treco específico pelo Id.
+    public static void listOne() {
+    }
+
+    // Cadastra um novo treco.
+    public static void newThing() {
+    }
+
+    // Edita um treco pelo Id.
+    public static void editThing() {
+    }
+
+    // Apaga um treco pelo Id.
+    public static void deleteThing() {
+    }
+
+    // Recarrega o menu principal.
+    public static void reloadMenu() {
+        clearScreen(); // Limpa o terminal.
+        System.out.println("Oooops! Opção inválida!\n");
+        mainMenu();    // Mostra o menu.
+    }
+
+    // Limpa a tela do terminal.
+    public static void clearScreen() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("\n");
+        }
+    }
+
 }
